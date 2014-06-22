5de57ef602a8ee4907f1286916f7d0c22717366e
npmiller <nicolas.pierre.miller@gmail.com>
2014-06-22 13:39:39 +0200

    Add a base template to insert rendered markdown in

--- weberl_views.erl
+++ weberl_views.erl
@@ -13,14 +13,16 @@ serve_files(BaseDir, FilePath) ->
 serve_md(BaseDir, FilePath) ->
 	FileMd = lists:concat([BaseDir, FilePath, ".md"]),
 	FileHtml = lists:concat([BaseDir, "-cache", FilePath, ".html"]),
+	{ok, Template} = file:read_file(lists:concat([BaseDir, "/base.html"])),
+	[Top, Bottom] = binary:split(Template, <<"-content-">>),
 	case { file:read_file_info(FileMd), file:read_file_info(FileHtml)} of
 		{{ok, MdInfo}, {ok, HtmlInfo}} -> if 
 		                                      MdInfo#file_info.mtime =< HtmlInfo#file_info.mtime -> 
-		                                          {ok, Content} = file:read_file(FileHtml), #response{content=Content};
+		                                          {ok, Content} = file:read_file(FileHtml), #response{content=[Top, Content, Bottom]};
 		                                      true -> 
-		                                          #response{content=renderMd(FileMd, FileHtml)} 
+		                                          #response{content=[Top, renderMd(FileMd, FileHtml), Bottom]} 
 		                                  end;
-		{{ok, _MdInfo}, {error, enoent}} -> #response{content=renderMd(FileMd, FileHtml)};
+		{{ok, _MdInfo}, {error, enoent}} -> #response{content=[Top, renderMd(FileMd, FileHtml), Bottom]};
 		{{error, enoent}, {_,_}} -> #response{status_code=404}
 	end.
 
