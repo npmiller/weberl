7b6ae1056c71fd24dd48e9c5ab8bb034c8ae723f
npmiller <nicolas.pierre.miller@gmail.com>
2014-06-23 22:20:38 +0200

    minor refactoring in the routes

--- src/weberl_routes.erl
+++ src/weberl_routes.erl
@@ -5,10 +5,15 @@
 
 route(Rq) ->
 	case Rq#request.url of
-		<<"/">> -> #response{content="Coucou"};
-		<<"/static", Path/binary>> -> weberl_views:serve_files("./static", bitstring_to_list(Path));
-		<<"/raw", Path/binary>> -> weberl_views:serve_files("./blog", lists:concat([bitstring_to_list(Path), ".md"]));
-		<<Path/binary>> -> weberl_views:serve_md("./blog", bitstring_to_list(Path));
-		_ -> #response{status_code=404}
+		<<"/">> ->
+			#response{content="Coucou"};
+		<<"/static", Path/binary>> ->
+			weberl_views:serve_files("./static", bitstring_to_list(Path));
+		<<"/raw", Path/binary>> ->
+			weberl_views:serve_files("./blog", [bitstring_to_list(Path), ".md"]);
+		<<Path/binary>> ->
+			weberl_views:serve_md("./blog", bitstring_to_list(Path));
+		_ ->
+			#response{status_code=404}
 	end.
 
